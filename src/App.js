2e76b956573621601b3d3da12b29336d4e1018fa
spacescript <simonbengoehring@gmail.com>
2022-09-27 14:59:01 -0700

    Calc Pack 1

--- src/App.js
+++ src/App.js
@@ -5,10 +5,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
+        <h3>
           Development is happening here!
-        </h1>
+        </h3>
       </header>
+      <div className="calculator" id="calculator">
+        <div className="display" id="display">
+        </div>
+        <div className="interface" id="interface">
+          <div className="clear button" id="clear"></div>
+          <div className="parenthesis button" id="parenthesis"></div>
+          <div className="percent button" id="percent"></div>
+          <div className="divide button" id="divide"></div>
+          <div className="seven button" id="seven"></div>
+          <div className="eight button" id="eight"></div>
+          <div className="nine button" id="nine"></div>
+          <div className="mulitiply button" id="multiply"></div>
+          <div className="four button" id="four"></div>
+          <div className="five button" id="five"></div>
+          <div className="six button" id="six"></div>
+          <div className="minus button" id="minus"></div>
+          <div className="one button" id="one"></div>
+          <div className="two button" id="two"></div>
+          <div className="three button" id="three"></div>
+          <div className="add button" id="add"></div>
+          <div className="color button" id="color"></div>
+          <div className="zero button" id="zero"></div>
+          <div className="decimal button" id="decimal"></div>
+          <div className="equals button" id="equals"></div>
+        </div>
+      </div>
     </div>
   );
 }

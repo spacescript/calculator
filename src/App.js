013c1084cf370128468cbf9a4a20cfcefac1edb2
spacescript <simonbengoehring@gmail.com>
2022-09-27 18:11:19 -0700

    Calc Pack 2

--- src/App.js
+++ src/App.js
@@ -4,35 +4,31 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>
-          Development is happening here!
-        </h3>
-      </header>
       <div className="calculator" id="calculator">
         <div className="display" id="display">
+          <p>Answer is:</p>
         </div>
         <div className="interface" id="interface">
-          <div className="clear button" id="clear"></div>
-          <div className="parenthesis button" id="parenthesis"></div>
-          <div className="percent button" id="percent"></div>
-          <div className="divide button" id="divide"></div>
-          <div className="seven button" id="seven"></div>
-          <div className="eight button" id="eight"></div>
-          <div className="nine button" id="nine"></div>
-          <div className="mulitiply button" id="multiply"></div>
-          <div className="four button" id="four"></div>
-          <div className="five button" id="five"></div>
-          <div className="six button" id="six"></div>
-          <div className="minus button" id="minus"></div>
-          <div className="one button" id="one"></div>
-          <div className="two button" id="two"></div>
-          <div className="three button" id="three"></div>
-          <div className="add button" id="add"></div>
+          <div className="clear button" id="clear"><p>C</p></div>
+          <div className="parenthesis button" id="parenthesis"><p>()</p></div>
+          <div className="percent button" id="percent"><p>%</p></div>
+          <div className="divide button" id="divide"><p>/</p></div>
+          <div className="seven button" id="seven"><p>7</p></div>
+          <div className="eight button" id="eight"><p>8</p></div>
+          <div className="nine button" id="nine"><p>9</p></div>
+          <div className="mulitiply button" id="multiply"><p>X</p></div>
+          <div className="four button" id="four"><p>4</p></div>
+          <div className="five button" id="five"><p>5</p></div>
+          <div className="six button" id="six"><p>6</p></div>
+          <div className="minus button" id="minus"><p>-</p></div>
+          <div className="one button" id="one"><p>1</p></div>
+          <div className="two button" id="two"><p>2</p></div>
+          <div className="three button" id="three"><p>3</p></div>
+          <div className="add button" id="add"><p>+</p></div>
           <div className="color button" id="color"></div>
-          <div className="zero button" id="zero"></div>
-          <div className="decimal button" id="decimal"></div>
-          <div className="equals button" id="equals"></div>
+          <div className="zero button" id="zero"><p>0</p></div>
+          <div className="decimal button" id="decimal"><p>.</p></div>
+          <div className="equals button" id="equals"><p>=</p></div>
         </div>
       </div>
     </div>
